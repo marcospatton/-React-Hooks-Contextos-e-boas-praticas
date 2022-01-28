7d77ab4beed3a5d838b5bc2120e94c46894e5393
Marco Antonio da Silva Francisco <marcospatton@hotmail.com>
2022-01-27 23:33:43 -0300

    Add files via upload

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,16 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import DadosPessoais from "./DadosPessoais";
 import DadosUsuario from "./DadosUsuario";
 import DadosEntrega from "./DadosEntrega";
 
 function FormularioCadastro({ aoEnviar, validarCPF }) {
-  return (
-    <>
-      <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF}/>
-      <DadosUsuario />
-      <DadosEntrega/>
-    </>
-  );
+  const [etapaAtual, setEtapaAtual] = useState(0);
+  const [dadosColetados, setDados] = useState({});
+ 
+  useEffect(()=>{
+    console.log(dadosColetados);
+  })
+
+  const formularios = [
+    <DadosUsuario aoEnviar={coletarDados} />,
+    <DadosPessoais aoEnviar={coletarDados} validarCPF={validarCPF} />,
+    <DadosEntrega aoEnviar={coletarDados} />,
+  ];
+
+  function coletarDados(dados){
+    setDados({...dadosColetados, ...dados});
+    proximo();
+  }
+  function proximo() {
+    setEtapaAtual(etapaAtual + 1);
+  }
+  return <>{formularios[etapaAtual]}</>;
 }
 
 export default FormularioCadastro;
